e781a0825c49a4e9edcc32bab2c82e7f969e0706
priya2267 <priyaragini2267@gmail.com>
2023-02-05 01:43:51 +0530

    Added new methods to TeamFinalizationProcedures.java Interface

--- src/main/java/TeamsInIPL/TeamFinalizationProcedures.java
+++ src/main/java/TeamsInIPL/TeamFinalizationProcedures.java
@@ -1,4 +1,12 @@
 package TeamsInIPL;
 
 public interface TeamFinalizationProcedures {
+    default void check_PlayersCount(){}
+    default int check_CountOfForeignPlayers(){
+        return 0;
+    }
+    default int check_PresenceOfWicketKeeper(){
+        return 0;
+    }
+
 }
